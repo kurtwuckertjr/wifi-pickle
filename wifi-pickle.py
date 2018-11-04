d86b8c06f959d0f91b5fb1abde87088f34bb3b5a
Shane Scott <sscott@gvit.com>
2018-11-04 15:59:43 -0600

    Updates to code license blocks, Changelog, etc

--- wifi-pickle.py
+++ wifi-pickle.py
@@ -3,7 +3,7 @@ from logging import getLogger,ERROR
 getLogger('scapy.runtime').setLevel(ERROR)
 
 """
-Author : Marcos Nesster - mh4root@gmail.com  PocL4bs Team
+Author : Shane W. Scott - sscott@gvit.com GoVanguard Inc.
 Licence : GPL v3
 
 Description:
