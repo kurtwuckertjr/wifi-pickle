80bc41a6f0358c479a52c1cc61f1255ef1b8700f
Shane Scott <sscott@gvit.com>
2018-12-14 09:13:58 -0600

    Add silent options to QT

--- deps/installPyQt4.sh
+++ deps/installPyQt4.sh
@@ -7,13 +7,14 @@ wget http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.12.3/PyQt4_gpl_x11-
 tar zxvf sip-4.19.13.tar.gz 
 tar zxvf PyQt4_gpl_x11-4.12.3.tar.gz
 cd sip-4.19.13/
-python3.6 configure.py -d /usr/local/lib/python3.6/site-packages --sip-module PyQt4.sip --target-py-version=3.6
+python3.6 configure.py -d /usr/local/lib/python3.6/site-packages --sip-module PyQt4.sip --target-py-version=3.6 
 make
 sudo make install
 make clean
 cd ..
 cd PyQt4_gpl_x11-4.12.3/
-python3.6 configure-ng.py -d /usr/local/lib/python3.6/site-packages --target-py-version=3.6
+python3.6 configure-ng.py -d /usr/local/lib/python3.6/site-packages --target-py-version=3.6 -silent -release -opensource -no-compile-examples -nomake examples -nomake t
+ests -no-qml-debug -confirm-license
 make
 sudo make install
 make clean
