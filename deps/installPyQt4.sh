cd457c4f38327224dd4379e8e327fa5781770b0f
Shane Scott <sscott@gvit.com>
2019-01-11 13:18:02 -0600

    Update CI

--- deps/installPyQt4.sh
+++ deps/installPyQt4.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+testForPython=`python --version`
+if [[ $testForPython == *"3.6"* ]]; then
+    pythonBin='python'
+else
+    pythonBin='python3.6'
+fi
+
 cd /tmp
 
 # Setup SIP and PyQt4
@@ -7,13 +14,13 @@ wget http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.12.3/PyQt4_gpl_x11-
 tar zxvf sip-4.19.13.tar.gz 
 tar zxvf PyQt4_gpl_x11-4.12.3.tar.gz
 cd sip-4.19.13/
-python3.6 configure.py -d /usr/local/lib/python3.6/site-packages --sip-module PyQt4.sip --target-py-version=3.6 
+$pythonBin configure.py -d /usr/local/lib/python3.6/site-packages --sip-module PyQt4.sip --target-py-version=3.6 
 make
-sudo make install
+make install
 make clean
 cd ..
 cd PyQt4_gpl_x11-4.12.3/
-python3.6 configure-ng.py --confirm-license -d /usr/local/lib/python3.6/site-packages --target-py-version=3.6
+$pythonBin configure-ng.py --confirm-license -d /usr/local/lib/python3.6/site-packages --target-py-version=3.6
 make
-sudo make install
+make install
 make clean
