db226e6f14b2fe9cd55a19b3b8f31a933fc1e346
Shane Scott <sscott@gvit.com>
2019-01-11 13:48:45 -0600

    Update CI

--- deps/installPyQt4.sh
+++ deps/installPyQt4.sh
@@ -6,6 +6,15 @@ else
     pythonBin='python3.6'
 fi
 
+travisCiPythonSP='/home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages'
+typicalPythonSP='/usr/local/lib/python3.6/site-packages'
+
+if [ -d "$travisCiPythonSP" ]; then
+    pythonSP=$travisCiPythonSP
+else
+    pythonSP=$typicalPythonSP
+fi
+
 cd /tmp
 
 # Setup SIP and PyQt4
@@ -14,13 +23,13 @@ wget http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.12.3/PyQt4_gpl_x11-
 tar zxvf sip-4.19.13.tar.gz 
 tar zxvf PyQt4_gpl_x11-4.12.3.tar.gz
 cd sip-4.19.13/
-$pythonBin configure.py -d /usr/local/lib/python3.6/site-packages --sip-module PyQt4.sip --target-py-version=3.6 
+$pythonBin configure.py -d $pythonSP --sip-module PyQt4.sip --target-py-version=3.6 
 make
 make install
 make clean
 cd ..
 cd PyQt4_gpl_x11-4.12.3/
-$pythonBin configure-ng.py --confirm-license -d /usr/local/lib/python3.6/site-packages --target-py-version=3.6
+$pythonBin configure-ng.py --confirm-license -d $pythonSP --target-py-version=3.6
 make
 make install
 make clean
