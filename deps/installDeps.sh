3bb5397d27f8b9a11fd9a16b6a2546a78e1e329c
Shane Scott <sscott@gvit.com>
2019-01-11 13:40:42 -0600

    Update CI

--- deps/installDeps.sh
+++ deps/installDeps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Install deps
 sudo apt-get update
-sudo apt-get install -y build-essential libreadline-gplv2-dev libncursesw5-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev python3-dev python-netlib libxml2-dev libxslt1-dev python-dev libnetfilter-queue-dev qt4-qmake libqt4-dev libsqlite3-dev
+sudo apt-get install -y build-essential libreadline-gplv2-dev libncursesw5-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev python3-dev python-netlib libxml2-dev libxslt1-dev python-dev libnetfilter-queue-dev qt4-qmake libqt4-dev libsqlite3-dev mlocate
+updatedb
+locate setuptools
